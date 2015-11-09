34c54ec20b5a0160d4357d850dbe04e0e0731b0b
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-09 08:54:53 -0800

    complete insertion sort

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,3 +1,28 @@
 var insertionSort = function() {
+  for (var i = 1; i < array.length; i++) {
+    insert(array, i-1, array[i]);
+  }
+  return array;
+}
+
+var insert = function(array, rightMostIndex, value) {
+  var key = value;
+
+  for (var i = rightMostIndex; i >= 0; i--) {
+    if (array[i] > key) {
+      array[i+1] = array[i];
+    }
+
+    if (array[i] <= key) {
+      array[i+1] = key;
+      break;
+      //what if I put return array here?
+    }
+
+    if (i === 0) {
+      array[i] = key;
+    }
+  }
   
+  return array;
 }
