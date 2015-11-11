e78c00affa7ea675f5041117e3518c0702591078
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-11 09:24:37 -0800

    refactor rockpaperscissors

--- src/rock_paper_scissors.js
+++ src/rock_paper_scissors.js
@@ -9,8 +9,29 @@
 
 function rockPaperPermutation (roundCount) {
 
+//base array for founds
 var rps = ['r','p','s'];
+//result from applyArray()
+var applied; 
 
+//helper function
+//apply every elt in arr1 to every elt in 
+//arr2 and push the result to a new array
+//that will be the size of 
+//arr1.length * arr2.length
+function applyArray(arr1, arr2) {
+  var result = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      result.push(arr1[i] + arr2[j]); 
+    }
+  }
+  return result;
+}
+
+//recursively apply the array
+//from the previous round
+//Counts the round down to zero.
 function playRPS(count, result) {
   if (count === 0) {
     return applyArray(result, []);
@@ -18,20 +39,8 @@ function playRPS(count, result) {
   if (count === 1) {
     return applyArray(result, ['']);
   }
-  var apply = applyArray(rps, result);
-  count = count-1;
-  return playRPS(count, apply);
-}
-
-function applyArray(arr1, arr2) {
-  var result = [];
-  var temp = [];
-  for (var i = 0; i < arr1.length; i++) {
-    for (var j = 0; j < arr2.length; j++) {
-      result.push(arr1[i] + arr2[j]); 
-    }
-  }
-  return result;
+  applied = applyArray(rps, result);
+  return playRPS(count-1, applied);
 }
 
 return playRPS(roundCount, rps);
