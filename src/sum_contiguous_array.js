80c4f2c4d47edd1cd43007eb2945e158fbef4dc4
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-20 09:21:59 -0800

    add more approaches to sum contiguous array

--- src/sum_contiguous_array.js
+++ src/sum_contiguous_array.js
@@ -1,7 +1,7 @@
 function sumArray(array) {
   var highestSum = array[0];
 
-  function traverse (startIndex) {
+  function traverseSubsetsAtIndexN (startIndex) {
     var current = array[startIndex];
 
     //single element 
@@ -19,7 +19,7 @@ function sumArray(array) {
   }
 
   for (var i = 0; i < array.length; i++) {
-    traverse(i);
+    traverseSubsetsAtIndexN(i);
   }
 
   return highestSum;
@@ -36,3 +36,58 @@ arr2 = [ 10, -11, 11 ];
 arr3 = [ 1, 2, 3, -6, 4, 5, 6 ];
 
 //sumArray(arr3);
+//another approach
+
+function sumArray01(array) {
+
+  var maxSum = Number.NEGATIVE_INFINITY;
+  var currentSum;
+
+  for (var start = 0; start < array.length; start++) {
+    currentSum =0;
+    for (var end = start; end < array.length; end++) {
+      currentSum += array[end];
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+      }
+    }
+  }
+
+  return maxSum;
+  
+}
+
+//another more efficient way of doing it
+//iterate through all endpoints, and for each 
+//one look at only the best interval
+
+//for each endpoint, max interval ending there 
+//either has one number (start point = previous point)
+//or a start point before the previous point.
+
+//in this case, the best start point before the previous
+//point is the same one we found
+
+
+//for each endpoint
+//max interval, either has one number 
+//or a start point 
+function sumArrayEfficient(array) {
+
+  var maxSum = Number.NEGATIVE_INFINITY;
+  var currentSum = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    //add new number to current sum
+    currentSum += array[i];
+    //record as the largest sum if necessary
+    maxSum = Math.max(maxSum, currentSum);
+    //if negative, throw out earlier progress
+    //and consider intervals starting at this point
+    if (currentSum < 0) {
+      currentSum = 0;
+    }
+  }
+  
+  return maxSum;
+}
