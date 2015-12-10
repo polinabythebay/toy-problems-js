864a28433a6769210eb31a7ff7184d61ad1266cd
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-10 09:59:17 -0800

    add more anagrams solutions

--- src/anagrams.js
+++ src/anagrams.js
@@ -4,9 +4,62 @@ all possible anagrams of a string and outputs them as an array
 
 This is similar to generating all permutations. Except in this case
 if there are words with repeated letters, you will end up with 
-repeated permutations. Ignore those and you've got your anagrams
+repeated permutations. Ignore those and you've got your anagrams.
+
+Is this actually combinations since we're not including redundancies?
 **************************************************************/
 
+/*************************************************************
+Solution #3
+
+**************************************************************/
+
+var allAnagrams = function(string) {
+  var result = {};
+  // Base case
+  if (string.length <= 1){
+    return [string];
+  }
+  // For each letter in string
+  string.split('').forEach(function(letter, i) {
+    var remainingLetters = string.slice(0, i) + string.slice(i + 1);
+    // For each anagram of the remaining letters
+    allAnagrams(remainingLetters).forEach(function(anagram) {
+      result[letter + anagram] = true;
+    });
+  });
+  return Object.keys(result);
+};
+
+
+/*************************************************************
+Solution #2
+Handles object that stores permutations more gracefully
+**************************************************************/
+
+function allAnagrams = function(string) {
+  var anagrams = {};
+  var generator = function(text, options) {
+    if (text.length === string.length) {
+      //overwrites repeated permutations
+      //so don't need to add a case here
+      anagrams[text] = true;
+    }
+
+    for (var i = 0; i < options.length; i++) {
+      generator(text + options[i], options.slice(0,i) + options.slice(i + 1));
+    }
+  }
+  generator('', string);
+  return Object.keys(anagrams);
+}
+
+
+/*************************************************************
+Solution #1
+Rotates characters after the recursive call
+Not entirely graceful/understandable
+**************************************************************/
 function generateAnagrams(string) {
   //turn input string to array
   var inputArr = string.split("");
---
+++ src/longest_palindrome.js
@@ -0,0 +1,7 @@
+/*************************************************************
+A function that finds the longest palindrome in a given string.
+This is similar to finding the powerset, except filter by the
+criteria that each subset must be a palindrome and each subset
+must be consecutive. So I guess not really a power set :p
+**************************************************************/
+
