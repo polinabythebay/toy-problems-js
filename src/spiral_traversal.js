79103edc0497636e912bb82ba400fde6d6928e65
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-11 11:58:07 -0800

    complete spiral traversal

--- src/spiral_traversal.js
+++ src/spiral_traversal.js
@@ -4,10 +4,12 @@ same-length arrays), print out every value found, but in a
 spiral from the upper left in to the center.
 **************************************************************/
 
-//input 2 dimensional array
-//four operations
 
-var arrs = 
+/*************************************************************
+Testing
+**************************************************************/
+
+var arrs1 = 
 [ [ 1, 2, 3 ], 
 [ 4, 5, 6 ], 
 [ 7, 8, 9 ], 
@@ -17,95 +19,112 @@ var arrs =
 [ 19, 20, 21 ], 
 [ 22, 23, 24 ] ];
 
-var output = [ 1, 2, 3, 6, 9, 12, 15, 18, 
-21, 24, 23, 22, 19, 16, 13, 
-10, 7, 4, 5, 8, 11, 14, 17, 20 ];
+var arrs1_output = [ 1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 23, 22, 19, 16, 13, 10, 7, 4, 5, 8, 11, 14, 17, 20 ];
 
-var result = [];
+var arrs2 = 
+[ [ 1, 2, 3, 4, 5 ], 
+[ 6, 7, 8, 9, 10 ], 
+[ 11, 12, 13, 14, 15 ], 
+[ 16, 17, 18, 19, 20 ], 
+[ 21, 22, 23, 24, 25 ] ];
 
-//given last index, starts at next
-//index to the right and keeps going right
-//num times
-var right = function(lastIndexRow, lastIndexCol, n) {
-  var indexRow = lastIndexRow;
-  var indexCol = lastIndexCol+1;
-  while(indexCol < n) {
-      console.log("position", arrs[indexRow][indexCol]);
-      result.push(arrs[indexRow][indexCol]);
+var arrs2_output = [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
+
+var arrs3 = [ [ 1 ], [ 2 ], [ 3 ], [ 4 ] ];
+
+var arrs3_output = [ 1, 2, 3, 4 ];
+
+var arrs4 = [ [ 1, 2, 3, 4, 5, 6, 7 ] ];
+
+var arrs4_output = [ 1, 2, 3, 4, 5, 6, 7 ];
+
+/*************************************************************
+//input 2 dimensional array
+//four operations: up, right, down, left
+**************************************************************/
+
+var spiralTraversal = function(matrix) {
+
+  var result = [];
+  var rightLimit = matrix[0].length;
+  var downLimit = matrix.length;
+  var leftLimit = -1;
+  var upLimit = 0;
+
+  //begin by going right
+  right(0,-1,rightLimit);
+
+  return result;
+    
+  function right(lastIndexRow, lastIndexCol) {
+    var indexRow = lastIndexRow;
+    var indexCol = lastIndexCol+1;
+    while(indexCol < rightLimit) {
+      result.push(matrix[indexRow][indexCol]);
       indexCol++;
+    }
+    rightLimit--;
+    indexCol--;
+
+    if(indexCol !== lastIndexCol) {
+      down(indexRow,indexCol,downLimit); 
+    } else {
+      return;
+    }
   }
-  indexCol--;
-  console.log("lastRow", indexRow, "lastCol", indexCol)
-}
 
+  function down(lastIndexRow, lastIndexCol) {
+    var indexRow = lastIndexRow+1;
+    var indexCol = lastIndexCol;
 
-//starts at next index down and keeps going
-//down n times
-var down = function(lastIndexRow, lastIndexCol, n) {
-   var indexRow = lastIndexRow+1;
-   var indexCol = lastIndexCol;
-   
-   while(indexRow < n) {
-      console.log("position", arrs[indexRow][indexCol]);
-      result.push(arrs[indexRow][indexCol]);
-      
+    while(indexRow < downLimit) {
+      result.push(matrix[indexRow][indexCol]);
       indexRow++;
-   }
-   indexRow--;
-   console.log("lastRow", indexRow, "lastCol", indexCol)
+    }
+    downLimit--;
+    indexRow--;
+
+    if(indexRow !== lastIndexRow) {
+      left(indexRow, indexCol, leftLimit);
+    } else {
+      return;
+    } 
+  }
+
+  function left(lastIndexRow, lastIndexCol) {
+    var indexRow = lastIndexRow;
+    var indexCol = lastIndexCol-1;
+
+    while(indexCol > leftLimit) {
+      result.push(matrix[indexRow][indexCol]);
+      indexCol--;
+    }
+    indexCol++;
+    leftLimit++;
+
+    if(indexCol !== lastIndexCol) {
+      up(indexRow, indexCol, upLimit);
+    } else {
+      return;
+    }
+  }
+
+  function up(lastIndexRow, lastIndexCol) {
+    var indexRow = lastIndexRow-1;
+    var indexCol = lastIndexCol;
+
+    while(indexRow > upLimit) {
+      result.push(matrix[indexRow][indexCol]);
+      indexRow--;
+    }
+    indexRow++;
+    upLimit++;
+
+    if (indexRow !== lastIndexRow) {
+      right(indexRow, indexCol, rightLimit);
+    } else {
+      return;
+    }
+  }
 }
-
-
-//starts at next index to the left and keeps
-//going left n times
-var left = function(lastIndexRow, lastIndexCol, n) {
-   var indexRow = lastIndexRow;
-   var indexCol = lastIndexCol-1;
-   
-   while(indexCol > n) {
-       console.log("position", arrs[indexRow][indexCol]);
-       result.push(arrs[indexRow][indexCol]);
-        indexCol--;
-   }
-   indexCol++;
-    console.log("lastRow", indexRow, "lastCol", indexCol)
-}
-
-//starts at next index above and keeps 
-//going up n times
-var up = function(lastIndexRow, lastIndexCol, n) {
-   var indexRow = lastIndexRow-1;
-   var indexCol = lastIndexCol;
-   
-   while(indexRow > n) {
-       console.log("position", arrs[indexRow][indexCol]);
-       result.push(arrs[indexRow][indexCol]);
-       indexRow--;
-   }
-   indexRow++;
-   console.log("lastRow", indexRow, "lastCol", indexCol);
-}
-
-var spiralTraverse = function() {
-    
-    //go through right, down, left, right
-    //update lastRow/lastCol
-    //if previous row/col == next stopping row/col, then stop
-    
-    right(0,-1,arrs[0].length);
-    down(0,2,arrs.length);
-    left(7,2, -1);
-    up(7,0, 0);
-    
-    right(1,0,arrs[0].length-1);
-    down(1,1,arrs.length-1);
-    
-    left(6,1,0); //did nothing (if pair === previous pair, stop)
-
-    
-    console.log(result);
-    
-}
-
-spiralTraverse();
 
