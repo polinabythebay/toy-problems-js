67fc3515368bd0af4fb35cdd7d0883cc387077ec
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-12 09:33:28 -0800

    add another version of spiral traversal

--- src/spiral_traversal.js
+++ src/spiral_traversal.js
@@ -5,6 +5,49 @@ spiral from the upper left in to the center.
 **************************************************************/
 
 /*************************************************************
+Another version using for loops instead of helper functions
+**************************************************************/
+
+var spiralTraversal = function(matrix){
+​
+  var results = [];
+  var startRowIndex = 0;
+  var endRowIndex = matrix.length-1;
+  var startColIndex = 0;
+  var endColIndex = matrix[0].length-1;
+​
+  while (startRowIndex <= endRowIndex && startColIndex <= endColIndex) {
+​
+    for (var i = startColIndex; i <= endColIndex; i++) {
+      results.push(matrix[startRowIndex][i]);
+    }
+    startRowIndex++;
+​
+    for (var j = startRowIndex; j <= endRowIndex; j++) {
+      results.push(matrix[j][endColIndex]);
+    }
+    endColIndex--;
+​
+    if (startRowIndex <= endRowIndex) {
+      for (var k = endColIndex; k >= startColIndex; k--) {
+        results.push(matrix[endRowIndex][k]);
+      }
+      endRowIndex--;
+    }
+​
+    if (startColIndex <= endColIndex) {
+      for (var m = endRowIndex; m >= startRowIndex; m--) {
+        results.push(matrix[m][startColIndex]);
+      }
+      startColIndex++;
+    }
+​
+  }
+​
+  return results;
+};
+
+/*************************************************************
 Testing
 **************************************************************/
 
