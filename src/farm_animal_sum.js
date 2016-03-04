22ac9f835b0466f64b11f9e8681f7f3fc19ad1d8
Polina Soshnin <psoshnin@wellesley.edu>
2016-03-04 15:27:40 -0800

    start farm animal sum

---
+++ src/farm_animal_sum.js
@@ -0,0 +1,20 @@
+// Write a program to determine the answer to the following question.
+
+// A farmer wants to spend exactly $100 to buy exactly 100 animals. 
+
+// The animals each cost the following:
+//     Cows: $10 
+//     Pigs: $3
+//     Chickens: 50¢
+
+// He must buy at least one of each. 
+// How many of each type of animal should he buy?
+
+//
+
+//similar to coin sum
+//given n, how many combinations sum up to n from
+//an array of numbers
+
+animals = [10,3,.5];
+
