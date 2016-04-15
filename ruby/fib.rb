75b61e2c1660adf932a53acdb5832a43116a34c0
Polina Soshnin <polina.soshnin@indiegogo.com>
2016-04-14 18:06:23 -0700

    add alt ruby fib version

--- ruby/fib.rb
+++ ruby/fib.rb
@@ -15,15 +15,28 @@ print fib(5);
 
 def nFib(n)
   x, y, sum = 0, 1, 1
-
   for i in 2..n
     sum = x + y
     x = y
     y = sum
   end
-  
   n === 0 ? 0 : sum
 end
+
+# alternative iterative solution
+
+def nthFib(n)
+  fib = [0,1]
+  for i in 2..n
+    fib[i] = fib[i-1] + fib[i-2]
+  end
+  fib[n]
+end
+
+
+
+
+
 
 
 
