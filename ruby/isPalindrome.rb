40ea386747f39ebce02d2c771d36f84743ae0692
Polina Soshnin <psoshnin@wellesley.edu>
2016-04-19 19:45:24 -0700

    add isPalindrome

---
+++ ruby/isPalindrome.rb
@@ -0,0 +1,11 @@
+  # var isPalindrome = function(left, right) {
+  #   while(left < right) {
+  #     if (arr[left] === arr[right]) {
+  #       left++;
+  #       right--;
+  #     } else {
+  #       return false;
+  #     }
+  #   }
+  #   return true;
+  # }
